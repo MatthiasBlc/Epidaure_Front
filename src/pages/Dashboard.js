04e4564615b50948dbb6012e2ced85c7713acac0
Olivia <oliviarudolf67@gmail.com>
2022-09-15 18:23:39 +0200

    add sidebar

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import GestionPraticienTitulaire from '../components/GestionPraticienTitulaire/GestionPraticienTitulaire';
 import Sidebar from '../components/Sidebar/Sidebar';
-import {FaTh} from 'react-icons/fa';
 
 const Dashboard = () => {
-    const menuItem=[
-        {
-            path: "/gestionPT",
-            name: "Gestion PT",
-            icon: <FaTh/>,
-        },
-        {
-            path: "/",
-            name: "",
-            icon: "",
-        },
-        {
-            path: "/",
-            name: "",
-            icon: "",
-        },
-    ]
 
     return (
-        <div>
+        <div className='flex gap-6'>
             <Sidebar />
             <GestionPraticienTitulaire />
         </div>
