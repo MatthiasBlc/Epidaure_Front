94723c2f185749baacd7f0607dde8cfa2526b936
Olivia <oliviarudolf67@gmail.com>
2022-09-15 14:36:24 +0200

    create files dashboard and gestionpraticien

---
+++ src/components/GestionPraticienTitulaire/GestionPraticienTitulaire.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GestionPraticienTitulaire = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default GestionPraticienTitulaire;
