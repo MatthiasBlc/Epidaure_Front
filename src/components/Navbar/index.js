280f17ca1019f169c47c8512da67186ef74daa5f
Olivia <oliviarudolf67@gmail.com>
2022-09-15 10:48:14 +0200

    navbar ok w/out burger menu responsive

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import useScrollPosition from "../../hooks/useScrollPosition";
-import logoGreen from '../../assets/images/logoEpiGreenFondNone.png'
-import logoWhite from '../../assets/images/logoEpiWhiteBgNone.png'
+import logoGreen from "../../assets/images/logoEpiGreenFondNone.png";
+import logoWhite from "../../assets/images/logoEpiWhiteBgNone.png";
 
 const Index = () => {
   function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
+    return classes.filter(Boolean).join(" ");
   }
-  
-  const scrollPosition = useScrollPosition()
 
+  const scrollPosition = useScrollPosition();
+
+  let activeStyle = {
+    textDecoration: "underline",
+    fontWeight: "bold",
+  };
 
   // var navMenuDiv = document.getElementById("nav-content");
   // var navMenu = document.getElementById("nav-toggle");
@@ -19,7 +23,6 @@ const Index = () => {
   // var navaction = document.getElementById("navAction");
   // var brandname = document.getElementById("brandname");
   // var toToggle = document.querySelectorAll(".toggleColour");
-
 
   // document.onclick = check;
   // function check(e) {
@@ -53,35 +56,25 @@ const Index = () => {
 
   return (
     <div>
-      <nav id="header" className={classNames(scrollPosition> 0? 'bg-white' : 'bg-transparent', 'fixed w-full z-30 top-0 text-white')}>
+      <nav
+        id="header"
+        className={classNames(
+          scrollPosition > 0 ? "bg-white" : "bg-transparent",
+          "fixed w-full z-30 top-0 text-white"
+        )}
+      >
         <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
           <div className="pl-4 flex items-center">
-            
-              {/* <svg
-                className="h-8 fill-current inline"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512.005 512.005"
-              >
-                <rect
-                  fill="#2a2a31"
-                  x="16.539"
-                  y="425.626"
-                  width="479.767"
-                  height="50.502"
-                  transform="matrix(1,0,0,1,0,0)"
-                />
-                <path
-                  className="plane-take-off"
-                  d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "
-                />
-              </svg> */}
-              <img className="h-20" src={classNames(scrollPosition> 0? logoGreen  : logoWhite)} alt="logo"/>
-            
+            <img
+              className="h-20"
+              src={classNames(scrollPosition > 0 ? logoGreen : logoWhite)}
+              alt="logo"
+            />
           </div>
           <div className="block lg:hidden pr-4">
             <button
               id="nav-toggle"
-              className="flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+              className="flex items-center p-1 text-green hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
             >
               <svg
                 className="fill-current h-6 w-6"
@@ -98,36 +91,41 @@ const Index = () => {
             id="nav-content"
           >
             <ul className="list-reset lg:flex justify-end flex-1 items-center">
-              <li className="mr-3">
-                <Link
+              <li className="mr-10">
+                <NavLink
+                  style={({ isActive }) => (isActive ? activeStyle : undefined)}
                   to="/"
-                  className="inline-block py-2 px-4 text-black font-bold no-underline"
                 >
-                  Active
-                </Link>
+                  Accueil
+                </NavLink>
               </li>
-              <li className="mr-3">
-                <Link
-                  to="/"
-                  className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+              <li className="mr-10">
+                <NavLink
+                  style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                  to="/profile"
                 >
-                  link
-                </Link>
+                  Profil
+                </NavLink>
               </li>
-              <li className="mr-3">
-                <Link
-                  to="/"
-                  className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+              <li className="mr-10">
+                <NavLink
+                  style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                  to="/Link"
                 >
-                  link
-                </Link>
+                  Lien
+                </NavLink>
               </li>
             </ul>
             <button
               id="navAction"
-              className={classNames(scrollPosition> 0? 'gradient text-white' : 'bg-white text-grey', 'mx-auto lg:mx-0 hover:underline font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out')}
+              className={classNames(
+                scrollPosition > 0
+                  ? "gradient text-white"
+                  : "bg-white text-grey",
+                "mx-auto lg:mx-0 hover:underline font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+              )}
             >
-              Action
+              Connexion
             </button>
           </div>
         </div>
