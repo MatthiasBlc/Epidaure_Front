a395ac133f641924220e9bf98592537b4da8ea08
Olivia <oliviarudolf67@gmail.com>
2022-09-15 16:01:45 +0200

    change cssconfig and fix footer

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Profile = () => {
     return (
         <div>
-            <button class="btn">Profile</button>
+            <h1 className='prof'>Profile</h1>
         </div>
     );
 };
