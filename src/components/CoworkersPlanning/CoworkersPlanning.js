f257f2dc8920bc2583f13b6435dff393f7b5fb35
Olivia <oliviarudolf67@gmail.com>
2022-09-16 18:00:51 +0200

    create component coworkers planning

---
+++ src/components/CoworkersPlanning/CoworkersPlanning.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CoworkersPlanning = () => {
+  return (
+    <div>
+      <h2 className="mt-8 my-2 text-1xl font-bold leading-tight text-left text-gray-800">
+        Gestion praticien titulaire
+      </h2>
+      <div className="w-full mb-4">
+        <div className="mb-10 h-1 gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+      </div>
+    </div>
+  );
+};
+
+export default CoworkersPlanning;
