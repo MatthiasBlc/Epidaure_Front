b07d43c4eb63ef73a827a744ab4275e76477e0eb
Olivia <oliviarudolf67@gmail.com>
2022-09-16 18:03:36 +0200

    add h2

--- src/components/CoworkersPlanning/CoworkersPlanning.js
+++ src/components/CoworkersPlanning/CoworkersPlanning.js
@@ -4,7 +4,7 @@ const CoworkersPlanning = () => {
   return (
     <div>
       <h2 className="mt-8 my-2 text-1xl font-bold leading-tight text-left text-gray-800">
-        Gestion praticien titulaire
+        Planning collaborateurs
       </h2>
       <div className="w-full mb-4">
         <div className="mb-10 h-1 gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
