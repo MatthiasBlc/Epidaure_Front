00d01cb21609121b775e19f97f3b0a1c251b3e02
Olivia <oliviarudolf67@gmail.com>
2022-09-16 14:27:52 +0200

    logo redirect dash when logged

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -62,7 +62,7 @@ const Navbar = ({ location }) => {
       <nav id="header" className="w-full z-30 top-0 text-white gradient">
         <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
           <div className="pl-4 flex items-center">
-            <Link to="/">
+            <Link to={ logged ? "/dashboard" : "/"}>
               <img className="h-20" src={logoWhite}></img>
             </Link>
           </div>
