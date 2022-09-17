c641112c34d87121567c86b770eb36f1f426c162
Olivia <oliviarudolf67@gmail.com>
2022-09-17 18:58:38 +0200

    tests dashboard fail

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import GestionPraticienTitulaire from '../components/GestionPraticienTitulaire/GestionPraticienTitulaire';
-import Sidebar from '../components/Sidebar/Sidebar';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import CoworkersPlanning from "../components/CoworkersPlanning/CoworkersPlanning";
+import GestionPraticienTitulaire from "../components/GestionPraticienTitulaire/GestionPraticienTitulaire";
+import MyPlanning from "../components/MyPlanning/MyPlanning";
+import RoomsPlanning from "../components/RoomsPlanning/RoomsPlanning";
+import Sidebar from "../components/Sidebar/Sidebar";
 
 const Dashboard = () => {
+  const location = useLocation();
 
-    return (
-        <div className='flex gap-6'>
-            <Sidebar />
-            <GestionPraticienTitulaire />
-        </div>
-    );
+  return (
+    <div className="flex gap-6">
+      <Sidebar />
+      <GestionPraticienTitulaire />
+    </div>
+  );
 };
 
 export default Dashboard;
