c54a42aaaf74ae6973d1d2b6ab74029c6d24428c
Olivia <oliviarudolf67@gmail.com>
2022-09-21 16:10:42 +0200

    alerts

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar/Sidebar";
 
 const Dashboard = () => {
+
   return (
     <div className="flex gap-6">
       <Sidebar />
