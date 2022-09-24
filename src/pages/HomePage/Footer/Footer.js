c1a567933f63b0945f36b6e7e79be36f9e6b22a7
MatthiasBlc <102999699+MatthiasBlc@users.noreply.github.com>
2022-09-24 10:17:36 +0200

    Revert "Revert "Privacy page""

--- src/pages/HomePage/Footer/Footer.js
+++ src/pages/HomePage/Footer/Footer.js
@@ -22,20 +22,20 @@ const Footer = () => {
               />
             </div>
             <div className="flex-1">
-              <p className="uppercase text-gray-500 md:mb-6">Legal</p>
+              <p className="uppercase text-gray-500 md:mb-6">Mentions légales</p>
               <ul className="list-reset mb-6">
                 <li className="mt-2 inline-block mr-2 md:block md:mr-0">
                   <Link
                     to="/privacy"
                     className="no-underline hover:underline text-gray-800 hover:text-orange-500"
                   >
-                    Privacy
+                    Politique de confidentialité
                   </Link>
                 </li>
               </ul>
             </div>
             <div className="flex-1">
-              <p className="uppercase text-gray-500 md:mb-6">Company</p>
+              <p className="uppercase text-gray-500 md:mb-6">Equipe</p>
               <ul className="list-reset mb-6">
                 {teams &&
                   teams.map((team, k) => (
